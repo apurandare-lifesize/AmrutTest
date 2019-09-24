5d087dd6ab1165ac10914682760b084649e4af20
Amrut p <amrutp@Amruts-MacBook-Pro.local>
2019-09-24 18:18:20 +0530

    removed sum42

--- sketch.js
+++ sketch.js
@@ -21,9 +21,6 @@ function answer() {
 function digital_root(n) {
   return (n-1)%9+1;
 }
-function sum42(a, b) {
-  return a + b + 42;
-}
 
 function anomalyCode(x) {
   return '5' + x - x;
@@ -33,7 +30,6 @@ module.exports = {
   sum: sum,
   sub: sub,
   prod: prod,
-  sum42: sum42,
   digital_root: digital_root,
   sayHelloTo: sayHelloTo,
   answer: answer,
