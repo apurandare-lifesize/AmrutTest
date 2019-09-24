b71aa6e6d9cd6d6b6dfb3379cdb9a53d07ad292d
Amrut p <amrutp@Amruts-MacBook-Pro.local>
2019-09-24 19:17:56 +0530

    added sum42 in master

--- sketch.js
+++ sketch.js
@@ -14,16 +14,16 @@ function prod(a, b) {
   return a * b;
 }
 
-function sum42(a, b) {
-  return a + b + 42;
-}
-
 function answer() {
   return 42;
 }
 
 function digital_root(n) {
   return (n-1)%9+1;
+}
+
+function sum42(a, b) {
+  return a + b + 42;
 }
 
 function anomalyCode(x) {
@@ -34,6 +34,7 @@ module.exports = {
   sum: sum,
   sub: sub,
   prod: prod,
+  sum42: sum42,
   digital_root: digital_root,
   sayHelloTo: sayHelloTo,
   answer: answer,
